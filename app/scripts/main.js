32c0c42229725c1ba51b5e94c689dd111fe915b5
Chelsea Franz <chelseafranz3@gmail.com>
2014-11-03 16:49:33 -0500

    can add new bars, working on search

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,42 @@
 var all_bars= new Bars();
+
+all_bars.fetch().done( function(){
+	var barsview = new BarsView({
+	collection: all_bars,
+	});
+});
+
+var self=this;
+
+$('#nameButton').on('click', function(a){
+	a.preventDefault;
+	var newBarName= $('#nameofBar').val();
+	var newBarLocation=$('#locationofBar').val();
+	var newBartype=$('#typeofBar').val();
+	var newBarSpecialties=$('#specialtiesofBar').val();
+
+	var newBar= new Bar({
+		name: newBarName,
+		location: newBarLocation,
+		type: newBartype,
+		specialties: newBarSpecialties
+
+	});
+	all_bars.add(newBar);
+	newBar.save();
+
+});
+
+$('#searchButton').on('click', function(){
+	var search= $('#searchInput').val();
+		
+		
+
+
+		if(search == 'edgewood'){
+			console.log('find');
+		}else{
+			console.log('what');
+		}
+
+});
--- app/scripts/views/bars_views.js
+++ app/scripts/views/bars_views.js
@@ -1,20 +1,26 @@
-var barsView= Backbone.Views.extend({
+var BarsView= Backbone.View.extend({
 
 	tagName: 'ul',
-	className: 'bar'
+	className: 'bar',
 
 	initialize: function(options){
 		this.render(options.collection)
-	};
+	},
 
 	render: function(collection){
 		var self= this;
 
 
-	var template= $('#barsList').html();
+	var template= $('#bars').html();
 	var rendered= _.template(template);
+
+	_.each(collection.models, function(x){
+
+		self.$el.append(rendered(x.attributes));
+	});
+
+	$('.listofBars').html(this.el);
+	return this;
 	}
-
-
 
 });
