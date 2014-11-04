0560e86c067cc4d8d1297ca6360641eebaf88a12
Chelsea Franz <chelseafranz3@gmail.com>
2014-11-04 17:36:21 -0500

    some styling, added ids to list items

--- app/scripts/views/bars_views.js
+++ app/scripts/views/bars_views.js
@@ -3,6 +3,11 @@ var BarsView= Backbone.View.extend({
 	tagName: 'ul',
 	className: 'bar',
 
+
+	events:{
+		'click li' : 'deleteBar'
+	},
+
 	initialize: function(options){
 		this.render(options.collection)
 	},
@@ -21,6 +26,12 @@ var BarsView= Backbone.View.extend({
 
 	$('.listofBars').html(this.el);
 	return this;
+	},
+
+	deleteBar: function(a){
+		a.preventDefault;
+		var id= $(a.target).attr('id');
+		console.log(id);
 	}
 
 });
